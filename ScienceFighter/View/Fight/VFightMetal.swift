1b7cf2a5a03931236172a2312f85073e06eea986
vauxhall <zero.griffin@gmail.com>
2017-01-20 10:34:52 -0600

    view metal init

--- ScienceFighter/View/Fight/VFightMetal.swift
+++ ScienceFighter/View/Fight/VFightMetal.swift
@@ -2,5 +2,23 @@ import MetalKit
 
 class VFightMetal:MTKView
 {
+    private weak var controller:CFight!
     
+    init(controller:CFight)
+    {
+        let device:MTLDevice? = MTLCreateSystemDefaultDevice()
+        
+        super.init(
+            frame:CGRect.zero,
+            device:device)
+        clipsToBounds = true
+        isUserInteractionEnabled = false
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = UIColor.clear
+    }
+    
+    required init(coder:NSCoder)
+    {
+        fatalError()
+    }
 }
