a43ae4dd899774a1e3759a3a990c8dc07f13b178
vauxhall <zero.griffin@gmail.com>
2017-01-20 12:20:39 -0600

    view metal optional init

--- ScienceFighter/View/Fight/VFight.swift
+++ ScienceFighter/View/Fight/VFight.swift
@@ -3,17 +3,24 @@ import UIKit
 class VFight:VView
 {
     private weak var controller:CFight!
-    private weak var viewMetal:VFightMetal!
+    private weak var viewMetal:VFightMetal?
     
     override init(controller:CController)
     {
         super.init(controller:controller)
         self.controller = controller as? CFight
         
-        let viewMetal:VFightMetal = VFightMetal(
-            controller:self.controller)
-        self.viewMetal = viewMetal
+        guard
+            
+            let viewMetal:VFightMetal = VFightMetal(
+                controller:self.controller)
         
+        else
+        {
+            return
+        }
+        
+        self.viewMetal = viewMetal
         addSubview(viewMetal)
         
         NSLayoutConstraint.equals(
--- ScienceFighter/View/Fight/VFightMetal.swift
+++ ScienceFighter/View/Fight/VFightMetal.swift
@@ -3,10 +3,20 @@ import MetalKit
 class VFightMetal:MTKView
 {
     private weak var controller:CFight!
+    private let commandQueue:MTLCommandQueue
     
-    init(controller:CFight)
+    init?(controller:CFight)
     {
-        let device:MTLDevice? = MTLCreateSystemDefaultDevice()
+        guard
+            
+            let device:MTLDevice = MTLCreateSystemDefaultDevice()
+        
+        else
+        {
+            return nil
+        }
+        
+        commandQueue = device.makeCommandQueue()
         
         super.init(
             frame:CGRect.zero,
@@ -21,4 +31,60 @@ class VFightMetal:MTKView
     {
         fatalError()
     }
+    
+    override func draw()
+    {
+        super.draw()
+        
+        guard
+        
+            let drawable:CAMetalDrawable = currentDrawable,
+            let passDescriptor:MTLRenderPassDescriptor = currentRenderPassDescriptor
+        
+        else
+        {
+            return
+        }
+        
+    }
 }
+/*
+
+let renderPassDescriptor = MTLRenderPassDescriptor()
+renderPassDescriptor.colorAttachments[0].texture = drawable.texture
+renderPassDescriptor.colorAttachments[0].loadAction = .Clear
+renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(red: 0.0, green: 104.0/255.0, blue: 5.0/255.0, alpha: 1.0)
+renderPassDescriptor.colorAttachments[0].storeAction = .Store
+
+let commandBuffer = commandQueue.commandBuffer()
+commandBuffer.addCompletedHandler { (commandBuffer) -> Void in
+    dispatch_semaphore_signal(self.bufferProvider.avaliableResourcesSemaphore)
+}
+
+let renderEncoder = commandBuffer.renderCommandEncoderWithDescriptor(renderPassDescriptor)
+//For now cull mode is used instead of depth buffer
+renderEncoder.setCullMode(MTLCullMode.Front)
+
+renderEncoder.setRenderPipelineState(pipelineState)
+renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, atIndex: 0)
+renderEncoder.setFragmentTexture(texture, atIndex: 0)
+if let samplerState = samplerState{
+    renderEncoder.setFragmentSamplerState(samplerState, atIndex: 0)
+}
+
+renderEncoder.setFragmentTexture(texture, atIndex: 0)
+if let samplerState = samplerState{
+    renderEncoder.setFragmentSamplerState(samplerState, atIndex: 0)
+}
+
+let nodeModelMatrix = self.modelMatrix()
+nodeModelMatrix.multiplyLeft(parentModelViewMatrix)
+
+let uniformBuffer = bufferProvider.nextUniformsBuffer(projectionMatrix, modelViewMatrix: nodeModelMatrix)
+
+renderEncoder.setVertexBuffer(uniformBuffer, offset: 0, atIndex: 1)
+renderEncoder.drawPrimitives(.Triangle, vertexStart: 0, vertexCount: vertexCount)
+renderEncoder.endEncoding()
+
+commandBuffer.presentDrawable(drawable)
+commandBuffer.commit()*/
