ec98926727d0c785c6d1c5f7f79d105bf246d2c8
vauxhall <zero.griffin@gmail.com>
2017-01-20 10:37:56 -0600

    view fight with view metal

--- ScienceFighter/View/Fight/VFight.swift
+++ ScienceFighter/View/Fight/VFight.swift
@@ -2,4 +2,27 @@ import UIKit
 
 class VFight:VView
 {
+    private weak var controller:CFight!
+    private weak var viewMetal:VFightMetal!
+    
+    override init(controller:CController)
+    {
+        super.init(controller:controller)
+        self.controller = controller as? CFight
+        
+        let viewMetal:VFightMetal = VFightMetal(
+            controller:self.controller)
+        self.viewMetal = viewMetal
+        
+        addSubview(viewMetal)
+        
+        NSLayoutConstraint.equals(
+            view:viewMetal,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
 }
