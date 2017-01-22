e186020b1a294bb1ab8a753c0a9922632677f97d
vauxhall <zero.griffin@gmail.com>
2017-01-21 20:45:03 -0600

    texture sampling

--- ScienceFighter/Model/Fight/Facing/MFightFacingPositive.swift
+++ ScienceFighter/Model/Fight/Facing/MFightFacingPositive.swift
@@ -5,7 +5,7 @@ class MFightFacingPositive:MFightFacing
     override func initialPosition() -> MFightPosition
     {
         let position:MFightPosition = MFightPosition(
-            positionX:-kInitialX,
+            positionX:-300,
             positionY:kInitialY)
         
         return position
