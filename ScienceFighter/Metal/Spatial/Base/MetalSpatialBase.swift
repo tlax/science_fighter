19dc93268729606f025da33ad5696972de84b3a4
vauxhall <zero.griffin@gmail.com>
2017-01-22 12:45:20 -0600

    metal spatial direction

--- ScienceFighter/Metal/Spatial/Base/MetalSpatialBase.swift
+++ ScienceFighter/Metal/Spatial/Base/MetalSpatialBase.swift
@@ -14,4 +14,38 @@ class MetalSpatialBase
     {
         self.vertexBuffer = vertexBuffer
     }
+    
+    init(
+        device:MTLDevice,
+        width:Float,
+        height:Float)
+    {
+        let width_2:Float = width / kHalf
+        let height_2:Float = height / kHalf
+        let top:Float = height_2
+        let bottom:Float = -height_2
+        let left:Float = -width_2
+        let right:Float = width_2
+        
+        let vertexTopLeft:MetalVertex = MetalVertex(
+            positionX:left,
+            positionY:top)
+        let vertexTopRight:MetalVertex = MetalVertex(
+            positionX:right,
+            positionY:top)
+        let vertexBottomLeft:MetalVertex = MetalVertex(
+            positionX:left,
+            positionY:bottom)
+        let vertexBottomRight:MetalVertex = MetalVertex(
+            positionX:right,
+            positionY:bottom)
+        
+        let vertexFace:MetalVertexFace = MetalVertexFace(
+            topLeft:vertexTopLeft,
+            topRight:vertexTopRight,
+            bottomLeft:vertexBottomLeft,
+            bottomRight:vertexBottomRight)
+        
+        vertexBuffer = device.generateBuffer(bufferable:vertexFace)
+    }
 }
