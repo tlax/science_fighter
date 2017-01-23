0e1da8edbc1e084b51186bac0594e3b5286f0cb8
vauxhall <zero.griffin@gmail.com>
2017-01-23 17:30:56 -0600

    refactor textures

--- ScienceFighter/Metal/Spatial/Scene/MetalSpatialScene.swift
+++ ScienceFighter/Metal/Spatial/Scene/MetalSpatialScene.swift
@@ -4,8 +4,8 @@ import MetalKit
 class MetalSpatialScene:MetalSpatialBaseTexturedPositive
 {
     let textures:MetalSpatialSceneTextures
-    private let kWidth:Float = 914
-    private let kHeight:Float = 434
+    private let kWidth:Float = 792
+    private let kHeight:Float = 396
     
     init(
         device:MTLDevice,
